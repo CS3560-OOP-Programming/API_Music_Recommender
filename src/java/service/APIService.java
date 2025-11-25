051c51485542dfc87aace582c40f22bd3d47d29d
G <gegigson.cpp.edu@gmail.com>
2025-11-24 21:50:43 -0800

    Changed the directory structure to match project description

--- src/java/service/APIService.java
+++ src/java/service/APIService.java
@@ -2,4 +2,10 @@ package service;
 
 public class APIService {
     //Api communication
+    public static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("communication mcstuff here");
+
+    }
 }
