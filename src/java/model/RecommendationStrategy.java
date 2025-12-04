f2211cb1b3f732c2a7b56b3317c95e411f6baa93
G <gegigson.cpp.edu@gmail.com>
2025-12-03 17:54:47 -0800

    set up AudioSimilarityStrategy.java

--- src/java/model/RecommendationStrategy.java
+++ src/java/model/RecommendationStrategy.java
@@ -1,6 +1,6 @@
 package model;
 
-import model.Track; //was com.musicrecommender.model.Track in the lab, should we change the layout of our project?
+import model.Track; //was com.musicrecommender.model.Track in the lab, should we change the layout of our project? Yeah, i think we should make it similar to the lab first and then branch off from there
 import java.util.List;
 
 /**
